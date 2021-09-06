a01707cd47484de68ac90cfcb085d420bab3594b
payalakare <Akarepayal99mca@gmail.com>
2021-09-06 22:18:09 +0530

    added checks

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,4 @@
 var firstname=document.getElementById("txtFirstName");
-var lastname=document.getElementById("txtLastName");
 var email=document.getElementById("txtEmail");
 var phone=document.getElementById("txtPhone");
 var image=document.getElementById("txtimg");
@@ -56,6 +55,8 @@ function validatePhone(event){
   }
 
 }
+
+//to format phone number is given format
 function formatPhoneNumber(e){
   if(document.getElementById("txtPhone").value!=undefined && document.getElementById("txtPhone").value!="" 
      && document.getElementById("txtPhone").value!=''){
@@ -63,40 +64,59 @@ function formatPhoneNumber(e){
    var y=(typeof(e.target)!=='undefined')?e.target:e[0];
    var x=y.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);      
    document.getElementById("txtPhone").value=!x[2] ? x[1] :'(' + x[1] + ') '
-                                               + x[2] + (x[3] ? '-' +x[3] :'');
+   + (x[2] ? '-' +x[2] :'') + (x[3] ? '-' +x[3] :'');
   }
 }
 function formatPhoneNumberonchange(){
- if(document.getElementById("txtPhone").value!=undefined && document.getElementById("txtPhone").value!="" 
+  if(phone.value.replace(/\D/g, "").length<=10){
+    if(document.getElementById("txtPhone").value!=undefined && document.getElementById("txtPhone").value!="" 
     && document.getElementById("txtPhone").value!=''){
   var txtValue=document.getElementById("txtPhone").value;
   var x=txtValue.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);      
   document.getElementById("txtPhone").value=!x[2] ? x[1] :'(' + x[1] + ') '
-                                              + x[2] + (x[3] ? '-' +x[3] :'');
+                                              + (x[2] ? '-' +x[2] :'') + (x[3] ? '-' +x[3] :'');
+  }
+ 
+ }
+ else{
+  document.getElementById("errorTelephone").innerHTML="Please enter only 10 digit phone number";
  }
 }
 
 
 //button
 function SubmitVal(){
-    var validateName = /^[A-Za-z ]{3,}$/;
-    var validatePhone = /^[0-9() -]+$/;
+    var validateName=/^[A-Za-z ]+$/;
     var allValidate=true;
-      if(!firstname.value.match(validateName))
+    
+    //Validate Name
+      if(firstname.value.match(validateName))
       {
+        var namearr=firstname.value.split(" ");
+        if(namearr!=undefined && namearr.length>=2){
+          for(var i=0;i<namearr.length;i++){
+            if(namearr[i].length<4){
+              allValidate=false;
+              document.getElementById("errorFirstName").innerHTML="Please enter valid name, each word should have alteast 4 charcters";
+              break;
+            }
+            else{
+              document.getElementById("errorFirstName").innerHTML="";
+            }
+          }
+        }
+        else{
+          allValidate=false;
+          document.getElementById("errorFirstName").innerHTML="Please enter valid name, each word should have alteast 4 charcters";
+        } 
+      }
+      else{
         allValidate=false;
-         document.getElementById("errorFirstName").innerHTML="Please enter valid name, it should have alteast 4 charcters";
-      }
-      else if(firstname.value.match(validateName)){
-        document.getElementById("errorFirstName").innerHTML="";
-      }
-      if(!lastname.value.match(validateName)){
-        allValidate=false;
-        document.getElementById("errorLastName").innerHTML="Please enter valid last name, it should have alteast 4 charcters";
-      }
-      else if(lastname.value.match(validateName)){
-        document.getElementById("errorLastName").innerHTML="";
-      }
+          document.getElementById("errorFirstName").innerHTML="Please enter valid name, each word should have alteast 4 charcters";
+        }
+
+
+  //Validate Email
      if(!validateEMail(email.value))
       {
           allValidate=false
@@ -105,20 +125,32 @@ function SubmitVal(){
       else if(validateEMail(email.value)){
         document.getElementById("errorEmail").innerHTML="";
       }
-      if(!phone.value.match(validatePhone)) {
-          allValidate=false;
-          document.getElementById("errorTelephone").innerHTML="Please enter valid phone number";
+
+  //Validate Phone
+      if((phone.value!=null || phone.value!="") && allValidate) {
+        var res = phone.value.replace(/\D/g, "");
+          if(res.length==10){
+             document.getElementById("errorTelephone").innerHTML="";
+            allValidate=true;
+           }
+          else{
+        allValidate=false;
+        document.getElementById("errorTelephone").innerHTML="Please enter only 10 digit phone number";
+           }
       }
-      else if(phone.value.match(validatePhone)){
-        document.getElementById("errorTelephone").innerHTML="";
+      else if(phone.value==null || phone.value==""){
+        allValidate=false;
+        document.getElementById("errorTelephone").innerHTML="Please enter only 10 digit phone number";
+
       }
 
+  //if every condition passed then it will go to nextx page
       if(allValidate){
         clearAllError();
         randomNumber= Math.floor(1000 + Math.random() * 9000);
         console.log(randomNumber);
         localStorage.setItem("randomNumber", randomNumber);  
-        localStorage.setItem("fullname",firstname.value +" "+ lastname.value);
+        localStorage.setItem("fullname",firstname.value);
         localStorage.setItem("phone",phone.value);
         console.log(localStorage.getItem("randomNumber"));
         alert("Your otp is-    " +localStorage.getItem("randomNumber"));
@@ -130,13 +162,15 @@ function SubmitVal(){
       }
 
 }
+
+//to cllear all errors
 function clearAllError(){
   document.getElementById("errorFirstName").innerHTML="";
-  document.getElementById("errorLastName").innerHTML="";
   document.getElementById("errorEmail").innerHTML="";
   document.getElementById("errorTelephone").innerHTML="";
 }
 
+//to show it on lael once page is ready
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         document.getElementById("lblFullName").innerHTML=localStorage.getItem("fullname");
@@ -145,8 +179,8 @@ document.onreadystatechange = () => {
   };
 
 
+//to validate entered otp
 function ValidateOTP(){
-
     var txtOTP=document.getElementById("enterOTP");
     if(txtOTP.value==localStorage.getItem("randomNumber")){
         location.href ="../HTML/Pixel6Home.html";
